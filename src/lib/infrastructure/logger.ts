1f6b42be065edeb74638eb279f213d92c8c5c365
adi <adi.sembiring@invoice2go.com>
2020-02-12 04:52:21 +1100

    added log

--- src/lib/infrastructure/logger.ts
+++ src/lib/infrastructure/logger.ts
@@ -3,7 +3,6 @@ import * as winston from 'winston';
 export const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
-  defaultMeta: { service: 'lambda-node' },
   transports: [new winston.transports.File({ filename: 'error.log', level: 'error' }), new winston.transports.File({ filename: 'combined.log' })],
 });
 
