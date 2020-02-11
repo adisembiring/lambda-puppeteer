e24a5fa81b9d90e369234345be7890ea15763535
adi <adi.sembiring@invoice2go.com>
2020-02-12 05:47:41 +1100

    fix s3 client

--- src/lib/gateways/s3-gateway.ts
+++ src/lib/gateways/s3-gateway.ts
@@ -1,10 +1,9 @@
 import * as AWS from 'aws-sdk';
-import * as AWSXRay from 'aws-xray-sdk';
 
 import { logger } from '../infrastructure/logger';
 
 export function upload(bucket: string, s3Key: string, file: Buffer | ArrayBuffer): Promise<string> {
-  const s3 = AWSXRay.captureAWSClient(new AWS.S3()) as AWS.S3;
+  const s3 = new AWS.S3();
   return s3
     .upload({
       Bucket: bucket,
